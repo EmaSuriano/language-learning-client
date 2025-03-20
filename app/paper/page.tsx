e40d24fd9c819607c61ef88d5db08b490577d761
Emanuel Suriano <emanuel.suriano@revolut.com>
2025-03-20 18:47:57 +0100

    add base Path

--- app/paper/page.tsx
+++ app/paper/page.tsx
@@ -3,15 +3,18 @@ import { Viewer, Worker } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export default function Home() {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
   const searchParams = useSearchParams();
   const language = searchParams.get("lang") || "en"; // Default to 'en' if not specified
+  const pathname = usePathname();
 
-  const fileUrl =
-    language === "es" ? "/static/pdfs/spanish.pdf" : "/static/pdfs/english.pdf";
+  const basePath = pathname.replace("/paper", "");
+  const fileName = language === "es" ? "spanish" : "english";
+
+  const fileUrl = `${basePath}/static/pdfs/${fileName}.pdf`;
 
   return (
     <main>
